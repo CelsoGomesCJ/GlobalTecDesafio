f3d594e681e9807bfb82a79001e5ba987f030cc9
CelsoGomes2222 <celsogomes22@gmail.com>
2020-05-23 21:40:59 -0300

    Ajusta classe de cpf responsável pela validação

--- GTec.Nucleo/GTec.Nucleo/Utilidades/CPF.cs
+++ GTec.Nucleo/GTec.Nucleo/Utilidades/CPF.cs
@@ -9,5 +9,40 @@ namespace GTec.Nucleo.Utilidades
         {
             Numero = cpf;
         }
+        public bool Valido => EhValido(Numero);
+        public override string ToString() => string.IsNullOrEmpty(Numero) ? string.Empty : Numero.Length == 14 ? Numero : Numero.Insert(9, "-").Insert(6, ".").Insert(3, ".");
+        public static bool EhValido(string cpf)
+        {
+            var multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            var multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            if (cpf.Length != 11) return false;
+
+            var tempCpf = cpf.Substring(0, 9);
+            var soma = 0;
+
+            for (int i = 0; i < 9; i++) soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            var resto = soma % 11;
+            if (resto < 2) resto = 0;
+            else resto = 11 - resto;
+
+            var digito = resto.ToString();
+            tempCpf += digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++) soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+
+            if (resto < 2) resto = 0;
+            else resto = 11 - resto;
+
+            digito += resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
     }
 }
