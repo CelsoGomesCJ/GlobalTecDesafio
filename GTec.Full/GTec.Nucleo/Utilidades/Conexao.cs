c6b3c7a9bc468d601f3cb592bc6e576d0a756761
CelsoGomes2222 <celsogomes22@gmail.com>
2020-05-24 13:37:48 -0300

    Cria conexão com base de dados heroku nuvem

--- GTec.Full/GTec.Nucleo/Utilidades/Conexao.cs
+++ GTec.Full/GTec.Nucleo/Utilidades/Conexao.cs
@@ -14,20 +14,38 @@ namespace GTec.Nucleo.Utilidades
             _stringConexao = CrieStringConexao();
         }
 
+        //String de conexão banco de dados Local
+        //private string CrieStringConexao()
+        //{
+
+        //    var sbConnectionString = new NpgsqlConnectionStringBuilder()
+        //    {
+        //        Host = "localhost",
+        //        Port = 5432,
+        //        Username = "postgres",
+        //        Password = "root",
+        //        Database = "globaltec",
+        //    };
+
+        //    return sbConnectionString.ToString();
+
+        //}
+
+        //String de conexão base de dados na nuvem Heroku
         private string CrieStringConexao()
         {
-
             var sbConnectionString = new NpgsqlConnectionStringBuilder()
             {
-                Host = "localhost",
+                Host = "ec2-35-169-254-43.compute-1.amazonaws.com",
                 Port = 5432,
-                Username = "postgres",
-                Password = "root",
-                Database = "globaltec",
+                Username = "plusagiidsboue",
+                Password = "a17f09f3748137a1924aa2ad07caf6f8f0a4e26f96049ca835e0afe749190c52",
+                Database = "dd5dh6iucquukh",
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
             };
 
             return sbConnectionString.ToString();
-
         }
 
         public DbConnection CrieConexao()
